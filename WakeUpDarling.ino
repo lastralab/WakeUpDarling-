a053d581388361d9fb6bed4a29c01a7217efd1ad
Tania Mol <mariametropolis8.5@gmail.com>
2017-01-29 17:12:46 -0700

    Add files via upload

--- WakeUpDarling.ino
+++ WakeUpDarling.ino
@@ -1,3 +1,5 @@
+
+
 //  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
 // / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
 // | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
@@ -5,14 +7,16 @@
 // 
 // WakeUpDarling!
 // 
-// Made by Tania Melina
+// Made by Tania Molina
 // License: CERN Open Hardware License
-// Downloaded from: https://circuits.io/circuits/3145505-wakeupdarling
-
+// Downloaded from: https://circuits.io/circuits/3126865-wakeupdarling
 
 #include <LiquidCrystal.h>
+#include <DS3231.h> //download it if is not in your library
 
+DS3231 rtc(SDA, SCL);
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
 int starttime;
 int activetime;
 int prevoustime = 0;
@@ -25,55 +29,41 @@ void setup()
   lcd.begin(16, 2);
   lcd.clear();
   
-  Serial.begin(9600);
-  
+  Serial.begin(115200);
+
   pinMode(11, INPUT);
-  digitalWrite(11, HIGH);
+  digitalWrite(11, LOW);
   pinMode(10, INPUT);
-  digitalWrite(10, HIGH); 
+  digitalWrite(10, LOW); 
   pinMode(9, OUTPUT);
   digitalWrite(9, LOW);
-  
-  starttime = millis()/1000;
+  rtc.begin();
+  rtc.setTime(19,30,00); //Real Time for the Module, example: (19:30:00)
+  lcd.setCursor(1,0);
+  lcd.print("Wake up Darling");
+  lcd.setCursor(0,1);
+  lcd.print(rtc.getTimeStr());
 }
+
 void loop()
 {
-  
-  if(digitalRead(10) == LOW)
-  
-  {
-    
-    lcd.setCursor(0,1);
-    lcd.print("Wake up Darling!");
-    lcd.setCursor(0,0);
-    hours++;
-    delay(250);
-  }
-    
-  else if (digitalRead(11) == LOW)
-    {
-     mins++;
-     delay(250);
-    } 
-  
-      activetime = (millis() / 1000) - starttime;
-      if(prevoustime < (activetime - 59))
+  activetime = (millis() / 1000) - starttime;
+      
+   if (prevoustime < (activetime - 59))
       {
        mins++;
        prevoustime = activetime;
-      } 
-      
-      if(mins > 59)
+      }  
+   if (mins > 59)
      {
       hours++;
       mins = 0;
      } 
-     
-     if(hours > 23)
+    
+   if(hours > 23)
      {
       hours = 0; 
      }
-  
   
   lcd.setCursor(6,0);
   
