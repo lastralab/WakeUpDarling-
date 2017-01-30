cf900e039fff1da88f4bcdd918b63e86af3f6c7a
Tania Mol <mariametropolis8.5@gmail.com>
2017-01-29 17:14:12 -0700

    Add files via upload

--- WakeUpDarling.ino
+++ WakeUpDarling.ino
@@ -1,101 +1,57 @@
-
-
-//  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
-// / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
-// | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
-// |_/___|___/___/   \___|___|_|_\\___|\___/|___| |_| |___/ 
-// 
 // WakeUpDarling!
-// 
+// Dedicated to my wife.
 // Made by Tania Molina
-// License: CERN Open Hardware License
+// License: MIT License
 // Downloaded from: https://circuits.io/circuits/3126865-wakeupdarling
 
+#include <DS3231.h> //download if you don't have it yet.
 #include <LiquidCrystal.h>
-#include <DS3231.h> //download it if is not in your library
 
-DS3231 rtc(SDA, SCL);
+DS3231  rtc(SDA, SCL);
+Time t;
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
-int starttime;
-int activetime;
-int prevoustime = 0;
-
-int hours = 0;
-int mins = 0;
 
 void setup()
 {
-  lcd.begin(16, 2);
-  lcd.clear();
-  
+  // Setup Serial connection
   Serial.begin(115200);
-
-  pinMode(11, INPUT);
-  digitalWrite(11, LOW);
-  pinMode(10, INPUT);
-  digitalWrite(10, LOW); 
   pinMode(9, OUTPUT);
   digitalWrite(9, LOW);
   rtc.begin();
-  rtc.setTime(19,30,00); //Real Time for the Module, example: (19:30:00)
-  lcd.setCursor(1,0);
-  lcd.print("Wake up Darling");
-  lcd.setCursor(0,1);
-  lcd.print(rtc.getTimeStr());
+
+  rtc.setTime(6, 59, 50);     // Set the real time for the module 
+  //<hour, minutes, seconds> (24hr format)
+  //rtc.setDate(1, 8, 2017);   // Uncomment to set the date to January 1st, 2014 (optional)
 }
 
 void loop()
 {
-  activetime = (millis() / 1000) - starttime;
-      
-   if (prevoustime < (activetime - 59))
-      {
-       mins++;
-       prevoustime = activetime;
-      }  
-   if (mins > 59)
-     {
-      hours++;
-      mins = 0;
-     } 
-    
-   if(hours > 23)
-     {
-      hours = 0; 
-     }
+  t = rtc.getTime();
+ 
+  lcd.begin(16, 2);
+  lcd.clear();
+  // Send time
+  lcd.setCursor(1,0);
+  lcd.print("Wake up Darling");
+  lcd.setCursor(0,1);
+  lcd.print(t.hour, DEC);
+  lcd.print("hrs ");
+  lcd.print(t.min, DEC);
+  lcd.print("min ");
+  lcd.print(t.sec, DEC);
+  lcd.print("sec");
+  delay(1000);
   
-  lcd.setCursor(6,0);
-  
-  if(hours < 10)
+  //set alarm:
+ 
+  if (t.hour == 7 && t.min == 0) //time to wake up
   {
-    lcd.print("0");
-    lcd.print(hours);
+    digitalWrite (9, HIGH); //turns on coffee machine
   }
-  else
+  if (t.hour == 9 && t.min == 0) //time to leave
   {
-    lcd.print(hours);
+    digitalWrite (9, LOW); //turn off coffee machine
   }
-    
-    lcd.print(":");
-    
-  if (mins < 10)
-  {
-    lcd.print("0");
-      lcd.print(mins);
-  }
-  else
-  {
-      lcd.print(mins);
-  }
-
-if (hours == 6 && mins == 0)
-{ 
-  digitalWrite (9, HIGH);  
-    } 
-if (hours == 9 && mins == 0)
-{
-  digitalWrite (9, LOW);
-}
   
 }
